892d3cd77cf62469cb0be955481f80c229a8ede8
Jose Bustillo <josebustillodealba@gmail.com>
2020-08-06 15:00:51 -0500

    feat: store user in DB with verification and password hashing

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,13 +1,33 @@
+const User = require("../models/User");
+const bcryptjs = require('bcryptjs');
+
 // Resolvers
 const resolvers = {
     Query: {
         getAny: () => "E"
     },
     Mutation: {
-        newUser: (_, { input }) => {
-            console.log(input);
-            
-            return "New User WIP"
+        newUser: async (_, { input }) => {
+            const {email, password} = input;
+
+            // Check if user exist
+            const userExists = await User.findOne({email});
+            if (userExists) {
+                throw new Error('User already exists');
+            }
+
+            // Hash password
+            const salt = bcryptjs.genSaltSync(10);
+            input.password = await bcryptjs.hash(password, salt);
+
+            try {
+                // Store in DB
+                const user = new User(input);
+                user.save();
+                return user;
+            } catch (error) {
+                console.log(Error);
+            }
         }
     }
 }
