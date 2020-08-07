73a5fb2037bd90b1c0e9f1370dd92934f816d03c
Jose Bustillo <josebustillodealba@gmail.com>
2020-08-07 18:10:36 -0500

    feat: userAuth and createToken functions

--- db/resolvers.js
+++ db/resolvers.js
@@ -1,5 +1,13 @@
 const User = require("../models/User");
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require("dotenv").config({path: '.env'});
+
+const createToken = (user, secret, expiresIn) => {
+    const { id, email, name, lastName } = user;
+
+    return jwt.sign({ id, email, name, lastName }, secret, { expiresIn });
+}
 
 // Resolvers
 const resolvers = {
@@ -28,6 +36,28 @@ const resolvers = {
             } catch (error) {
                 console.log(Error);
             }
+        },
+        userAuth: async (_, { input }) => {
+            const { email, password } = input;
+
+            // if user exists
+            const userExists = await User.findOne({email});
+            if (!userExists) {
+                throw new Error("User doesn's exists");
+            }
+
+            // Check password
+            const rightPassword = await bcryptjs.compare(password, userExists.password);
+
+            if (!rightPassword) {
+                throw new Error("Password is wrong");
+            }
+
+            // Create token
+            return {
+                token: createToken(userExists, process.env.SECRET, '24h')
+            }
+
         }
     }
 }
